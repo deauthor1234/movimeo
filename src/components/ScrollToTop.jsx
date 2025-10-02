a49fb4f39cf5cabd8a1e3c349125bfccbb00e3ba
Oladapo Authority <dimejioladapo573@gmail.com>
2025-10-02 03:55:22 +0100

    Made scroll-to-top smoother

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -1,17 +1,25 @@
 import { BiUpArrowAlt } from "react-icons/bi"
 import { useEffect, useState } from "react";
 import { useMovieContext } from "../contexts/MovieContext";
+import { animateScroll as scroll } from "react-scroll";
 
 const ScrollToTop = () => {
     const [scrollableState, setScrollableState] = useState(false)
     const { loading, favorites } = useMovieContext()
+
+    const scrollToTop = () => {
+        scroll.scrollToTop({
+            duration: 500,
+            smooth: true,
+        });
+    };
     
     useEffect(() => {
         document.documentElement.scrollHeight > document.documentElement.clientHeight && setScrollableState(true)
     }, [loading, favorites])
 
     return (
-        scrollableState && <div className="scroll-to-top-btn" onClick={() => scrollTo(0, 0)} data-aos="fade-up">
+        scrollableState && <div className="scroll-to-top-btn" onClick={scrollToTop} data-aos="fade-up">
             <BiUpArrowAlt className="btn-ic" />
         </div>
     )
